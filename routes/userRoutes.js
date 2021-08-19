a196a23d9cd894d58c36113f46e0c67e8cdc1736
Marcus Allen <emkay1993@gmail.com>
2021-08-19 10:50:40 +0100

    Add email functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,22 @@ router.post(
 
 router.post('/login', authController.login);
 
+router.post(
+  '/forgotPassword',
+  authController.forgotPassword
+);
+
+router.patch(
+  '/resetPassword/:token',
+  authController.resetPassword
+);
+
+router.patch(
+  '/updateMyPassword/',
+  authController.protect,
+  authController.updatePassword
+);
+
 // Routes
 
 router
